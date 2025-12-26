1076b3f7eca6f7aa155d09fa3e4446a748531628
Ruslan <ruslan.effect@gmail.com>
2025-12-26 21:58:30 +0600

    Убраны пароли из client.properties и добавлена инструкция по ручной установке
    - Пароли админа убраны из client.properties (только комментарии)
    - Создан MANUAL_INSTALL.md с подробной инструкцией ручной установки
    - Описаны 2 варианта: локальная установка и через сетевую папку
    - Добавлены инструкции по автозагрузке и созданию ярлыков
    
    Для production можно:
    1. Раскомментировать пароли в client.properties (быстро, но небезопасно)
    2. Использовать переменные окружения ADMIN_PASSWORD (безопасно)

--- src/main/java/incuat/kg/svetoofor/TrafficLightApp.java
+++ src/main/java/incuat/kg/svetoofor/TrafficLightApp.java
@@ -409,8 +409,8 @@ public class TrafficLightApp extends Application {
         switch (message) {
             case "RED_BLINK" -> blinkColor(redCircle, Color.RED, 120, "red");
             case "YELLOW_BLINK" -> blinkColor(yellowCircle, Color.YELLOW, 120, "yellow");
-            case "GREEN_BLINK_INCIDENT" -> blinkColor(redCircle, Color.LIMEGREEN, 30, "green_incident");  // Зеленый в кружке инцидента
-            case "GREEN_BLINK_ALERT" -> blinkColor(yellowCircle, Color.LIMEGREEN, 30, "green_alert");  // Зеленый в кружке алерта
+            case "GREEN_BLINK_INCIDENT" -> blinkColor(redCircle, Color.LIMEGREEN, 100, "green_incident");  // Зеленый в кружке инцидента
+            case "GREEN_BLINK_ALERT" -> blinkColor(yellowCircle, Color.LIMEGREEN, 100, "green_alert");  // Зеленый в кружке алерта
             case "QUEUE_RED" -> setQueueColor(greenCircle, Color.RED, "queue_red");
             case "QUEUE_GREEN" -> setQueueColor(greenCircle, Color.LIMEGREEN, "queue_green");
         }
